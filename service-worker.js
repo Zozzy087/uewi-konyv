6901b0bb753a179858f0d3888202a2bbfe30a495
DARK_FANTASY_KALANDKONYVEK <lughlleu@gmail.com>
2025-04-06 18:03:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,19 @@
-const CACHE_NAME = 'uewi-cache-v1';
+const CACHE_NAME = 'uewi-cache-v2';
 const urlsToCache = [
   'index.html',
+  'manifest.json',
+  'style/style.css',
+  'style/font-awesome.min.css',
+  'style/loading.css',
+  'javascript/config.js',
+  'javascript/fliphtml5-book.min.js',
+  'javascript/jquery-3.6.0.min.js',
+  'slide_javascript/slide.js',
   'files/shot.jpg',
+  'files/logo.png'
 ];
 
+// Telepítés: fájlok cache-elése
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
@@ -12,6 +22,7 @@ self.addEventListener('install', function(event) {
   );
 });
 
+// Kérések kezelése
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
