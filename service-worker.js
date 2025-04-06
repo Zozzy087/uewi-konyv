8160e63e2edfb5b0f05e1f46cfda9497e0adc589
DARK_FANTASY_KALANDKONYVEK <lughlleu@gmail.com>
2025-04-06 19:16:02 +0200

    d

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,31 @@
-const CACHE_NAME = 'uewi-final-cache-v1';
+const CACHE_NAME = 'uewi-konyv-cache-v1';
 const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "style/style.css",
-  "style/font-awesome.min.css",
-  "style/loading.css",
-  "javascript/config.js",
-  "javascript/fliphtml5-book.min.js",
-  "javascript/jquery-3.6.0.min.js",
-  "slide_javascript/slide.js",
-  "files/shot.jpg",
-  "files/logo.png"
+  '/',
+  '/index.html',
+  '/style/style.css',
+  '/javascript/config.js',
+  '/javascript/fliphtml5-book.min.js',
+  '/javascript/jquery-3.6.0.min.js',
+  '/slide_javascript/slide.js',
+  '/files/shot.jpg',
+  '/files/logo.png',
+  // Ide add hozzá a további szükséges fájlokat
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
